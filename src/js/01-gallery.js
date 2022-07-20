f94bed826ba99510d0dfb64af3b35c04f9557797
uncle-Stas <stanislav.zavadskyi11@gmail.com>
2022-07-20 21:20:38 +0300

    task1 completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+function createItems(array) {
+  return array.reduce((akum, item) => {
+    return (
+      akum +
+      `<a class="gallery__item" href=${item.original}>
+        <img
+          class="gallery__image"
+          src=${item.preview}
+          alt=${item.description}
+        />
+      </a>`
+    );
+  }, '');
+}
+
+const galleryItemsHtml = createItems(galleryItems);
+gallery.insertAdjacentHTML('afterbegin', galleryItemsHtml);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+  captionsData: 'alt',
+  captionDelay: 250,
+});
