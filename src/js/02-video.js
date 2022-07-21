a1cbe899d8b89a8c88f1035e673143664927ee5f
uncle-Stas <stanislav.zavadskyi11@gmail.com>
2022-07-21 19:06:34 +0300

    task2 completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@Vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.on(
+  'timeupdate',
+  throttle(currentTime => {
+    localStorage.setItem('videoplayer-current-time', currentTime.seconds);
+    console.log(currentTime.seconds);
+  }, 1000)
+);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
