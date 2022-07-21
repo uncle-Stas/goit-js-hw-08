d1fb8d114ec7396b5152fe0c9cd833046110dc89
uncle-Stas <stanislav.zavadskyi11@gmail.com>
2022-07-21 22:34:21 +0300

    task3 is done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+
+const complitedForm = {};
+
+feedbackForm.addEventListener(
+  'input',
+  throttle(event => {
+    if (event.target.name === 'email') {
+      complitedForm.email = event.target.value;
+      console.log(complitedForm);
+    }
+
+    if (event.target.name === 'message') {
+      complitedForm.message = event.target.value;
+      console.log(complitedForm);
+    }
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(complitedForm));
+  }, 500)
+);
+
+const feedbackInputEmail = feedbackForm.querySelector('input');
+const feedbackTextAria = feedbackForm.querySelector('textarea');
+
+if (localStorage.getItem('feedback-form-state') !== null) {
+  const feedbackFormState = JSON.parse(
+    localStorage.getItem('feedback-form-state')
+  );
+
+  feedbackInputEmail.value = feedbackFormState.email;
+  feedbackTextAria.value = feedbackFormState.message;
+}
+
+feedbackForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const feedbackFormState = JSON.parse(
+    localStorage.getItem('feedback-form-state')
+  );
+
+  console.log('complited form ', feedbackFormState);
+
+  feedbackInputEmail.value = '';
+  feedbackTextAria.value = '';
+
+  localStorage.clear();
+});
